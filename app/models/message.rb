4a59977d7ed7cd7d0f906ce6c1a16b702f00198d
Axis Sivitz <aosivitz@gmail.com>
2011-02-08 16:28:56 -0500

    Add URL auto linking

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,13 @@
 class Message < ActiveRecord::Base
     validates_presence_of :poster, :content
     belongs_to :channel
+
+    def marked_up_content
+        msg = self.content
+        matches = msg.scan(/http:\/\/\S+/)
+        matches.uniq.each do |word|
+            msg.gsub!(word, "<a href=\"#{word}\">#{word}</a>")
+        end
+        msg
+    end
 end
