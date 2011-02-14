0631f15f4090a958e9e0afbef371f5b8d898c59a
Axis Sivitz <aosivitz@gmail.com>
2011-02-14 15:33:07 -0500

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => 'channels'
 end
